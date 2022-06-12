7157a9b4d3a656380a89ad6cf89e7fe15be9363b
Katerina Zahradova <38363040+katz313@users.noreply.github.com>
2022-06-12 17:13:52 +0100

    Create angles.py

---
+++ angles.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 30 19:47:01 2020
+
+@author: Katka
+"""
+
+import numpy as np
+
+gm = np.pi*(5**0.5-1)/4
+sm = np.pi*(2**0.5-1)/4
+pipi = np.pi*np.pi/8
+sq2 = np.pi*(2**0.5)/4
+sq3 = np.pi*(3**0.5)/4
+
+pi2 = np.pi / 2 
+
+ln3 = np.pi*np.log(3)/3
+
+log3 = np.pi * np.log10(3)/2
